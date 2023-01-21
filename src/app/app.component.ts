9717209e9709113518876998ec68495bc021399d
Vishnu Vinod <vishnuvinod2772001@gmail.com>
2023-01-22 01:43:33 +0530

    working chat app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ChatService } from './chat.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  title = 'IE-Chat';
+export class AppComponent implements OnInit {
+  
+
+  public userName: string = '';
+  showChat = false;
+  message = '';
+
+  constructor(
+    public chats: ChatService
+  ) { }
+
+  ngOnInit() {
+    new ResizeObserver(el => {
+      setTimeout(() => {
+        document.body.scrollIntoView({ behavior: 'smooth', block: 'end' });
+      });
+    }).observe(document.body)
+  }
+
+  onStart() {
+    if (this.userName.length) {
+      this.showChat = true;
+      this.chats.getData();
+      this.chats.listenChanges();
+      this.chats.loading = true
+    }
+  }
+
+  onSend() {
+    if (this.message) {
+      this.chats.addOne(this.message, this.userName);
+      this.message = '';
+      this.chats.loading = true
+    }
+  }
+
+  onDelete(id: any) {
+    this.chats.delete(id)
+    this.chats.loading = true
+  }
+  
 }
---
+++ src/app/chat.service.ts
@@ -0,0 +1,78 @@
+import { Injectable } from '@angular/core';
+import * as Realm from "realm-web";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChatService {
+  public app = new Realm.App({ id: 'ie-chat-tweem' });
+  public ObjectId = Realm.BSON.ObjectId;
+  public loading = false;
+
+  public messages: any[] = [];
+
+  constructor() {
+    this.login();
+  }
+
+  public async login() {
+    // Create an anonymous credential
+    const credentials = Realm.Credentials.anonymous();
+
+    // Authenticate the user
+    const user = await this.app.logIn(credentials);
+    // `App.currentUser` updates to match the logged in user
+    console.assert(user.id === this.app?.currentUser?.id);
+  }
+
+  public delete(id: Realm.BSON.ObjectId) {
+    const collection = this.getCollection()
+    collection?.deleteOne({
+      _id: id
+    }).then(res => {
+      console.log(res)
+    }).catch(err => {
+      console.log(err)
+    })
+  }
+
+  public async getData() {
+    const collection = this.getCollection()
+    collection?.find().then(res => {
+      this.messages = res;
+      console.log(this.messages)
+      this.loading = false;
+    }).catch(err => {
+      console.log(err)
+    })
+  }
+
+  private getCollection() {
+    const mongo = this.app?.currentUser?.mongoClient('ie-chat');
+    const collection = mongo?.db('iechat').collection('chats');
+    return collection;
+  }
+
+  public addOne(message: string, userName: string) {
+    const mongo = this.app?.currentUser?.mongoClient('ie-chat');
+    const collection = mongo?.db('iechat').collection('chats');
+    collection?.insertOne({
+      message,
+      userName
+    }).then(res => {
+      console.log(res)
+    }).catch(err => {
+      console.log(err)
+    })
+  }
+
+  public async listenChanges() {
+    const collection = this.getCollection()
+    const obj = collection?.watch();
+    if (!obj) return;
+    for await (const change of obj) {
+      this.getData();
+    }
+  }
+
+}
